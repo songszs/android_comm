9253496a349477338e3a9736668b7af34d8d4487
zang_song <zang_song@gowild.cn>
2018-04-25 20:15:35 +0800

    1. 测试提交

--- mytest/app/src/main/java/com/zs/base/router/Router.java
+++ mytest/app/src/main/java/com/zs/base/router/Router.java
@@ -1,6 +1,10 @@
 package com.zs.base.router;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.HashMap;
 
 /**
  * @author: ZangSong
@@ -10,13 +14,65 @@ import android.content.Context;
  */
 public class Router {
 
-    public static final String URL_LOGIN_EVENTBUS = "url_login_eventbus";
+    public static final String URL_LOGIN_EVENTBUS  = "url_login_eventbus";
     public static final String URL_LOGIN_EVENTBUS_ = "url_login_eventbus";
 
+    public HashMap<String, RouterItem> routerMap;
 
-    public void startActivity(Context context, String url)
-    {
+    public Router() {
+        routerMap = new HashMap<>();
+    }
 
 
+    public void startActivity(Context context, String url) {
+
+
+    }
+
+    public void map(String url, RouterItem item) {
+        routerMap.put(url, item);
+    }
+
+    public void open(Context context, String url, Bundle bundle, String bundleKey) {
+
+        RouterItem item = routerMap.get(url);
+        Class activity = item.getActivity();
+        Class fragment = item.getFragment();
+
+        Intent intent = new Intent();
+        intent.setClass(context,activity);
+
+        if(bundle != null)
+        {
+            intent.putExtra(bundleKey,bundle);
+        }
+        context.startActivity(intent);
+    }
+
+
+    public class RouterItem {
+        private Class activity;
+        private Class fragment;
+
+        public RouterItem(Class activity, Class fragment) {
+            this.activity = activity;
+            this.fragment = fragment;
+        }
+
+        public Class getActivity() {
+            return activity;
+        }
+
+        public void setActivity(Class activity) {
+            this.activity = activity;
+        }
+
+        public Class getFragment() {
+            return fragment;
+        }
+
+        public void setFragment(Class fragment) {
+            this.fragment = fragment;
+        }
     }
 }
